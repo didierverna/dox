669d5bcfcf185e583f4bc3ca6ab4b9669944527d
Didier Verna <didier@didierverna.net>
2017-01-22 21:16:51 +0100

    Upgrade headers and license to LPPL 1.3.

--- dox.el
+++ dox.el
@@ -1,31 +1,22 @@
 ;;; dox.el --- AUC-TeX style file for DoX
 
-;; Copyright (C) 2009, 2010 Didier Verna
+;; Copyright (C) 2009, 2010, 2017 Didier Verna
 
-;; Author:        Didier Verna <didier@lrde.epita.fr>
-;; Maintainer:    Didier Verna <didier@lrde.epita.fr>
-;; Created:       Fri Sep 11 11:24:16 2009
-;; Last Revision: Sat Sep 19 09:54:43 2009
-;; Keywords:      tex abbrev data
-
+;; Author: Didier Verna <didier@didierverna.net>
+;; Keywords: tex abbrev data
 
 ;; This file is part of DoX.
 
-;; DoX may be distributed and/or modified under the
-;; conditions of the LaTeX Project Public License, either version 1.1
-;; of this license or (at your option) any later version.
-;; The latest version of this license is in
-;; http://www.latex-project.org/lppl.txt
-;; and version 1.1 or later is part of all distributions of LaTeX
-;; version 1999/06/01 or later.
+;; DoX may be distributed and/or modified under the conditions of the LaTeX
+;; Project Public License, either version 1.3 of this license or (at your
+;; option) any later version.  The latest version of this license is in
+;; http://www.latex-project.org/lppl.txt and version 1.3 or later is part of
+;; all distributions of LaTeX version 2005/12/01 or later.
 
-;; DoX consists of all files listed in the file `README'.
+;; DoX consists of all files listed in the file `README.md'.
 
 
 ;;; Commentary:
-
-;; Contents management by FCM version 0.1.
-
 
 
 ;;; Code:
